6cee275e5ec5e93d04319442f759cc5e31f3bcfb
rose <rosiek8@me.com>
2023-03-03 22:15:48 -0600

    commit

--- week11.js
+++ week11.js
@@ -20,7 +20,7 @@ let messageContainer=$("#Message");
 
 let board=["","","","","","","","","",];
 
-let message="Welcome to Tic Tac Toe";
+let message="Lets Play";
 
 let turn= 1;
 
@@ -35,6 +35,7 @@ $(function() {
 function renderBoard(){
     boardContainer.empty()
     for (let i = 0; i < board.length; i++) {
+        console.log("index:", i, board)
         const squareValue = board[i];
         renderSquare(i, squareValue);
     }
@@ -58,22 +59,47 @@ function renderMessage(){
 
 }
 
+// figuring out who is winning-what combinations
+//checking for turn what does that even mean
+//checking the information in each cell 
+function checkWinner() {
+    console.log("Checking for winner...")
+    let winningCombo = [[1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7]];
+    for (let index = 0; index < winningCombo.length; index++) {
+        console.log(index, "winning combo 1st loop", winningCombo[index]);
+        // second loop - Begin
+        for (let index2 = 0; index2 < winningCombo[index].length;index2++) {
+            console.log("winning combo 2nd loop", winningCombo[index][index2]);
+            // comparison of combinations to squares that have been played
+        }
+        
+        // second loop - End
+        
+    }
+}
+
 function onSquareClick(index){
+    console.log("index:", typeof index, index);
     if(turn===1) {
         board[index]= "X"
+        console.log(board[index]);
         turn=2
     } else {
         turn=1
         board[index]= "O"
+        console.log(board[index]);
     }
     // check if game is over --- render the board, game is over, we don't want the event listeners
-    if (board[3]==="X"){
-        gameOver=true
-        message="Game is over"
-    }
+    // if (board[1]==="X"){
+    //     gameOver=true
+    //     message="Game is over"
+    // } 
+    
 
     renderBoard()
     renderMessage()
+
+    checkWinner()
 
 }
 
@@ -90,3 +116,4 @@ function onResetClick(){
     renderBoard()
 
 }
+
